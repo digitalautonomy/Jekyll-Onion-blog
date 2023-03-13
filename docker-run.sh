1c7117ca81bef278917f6cb127e5e8c5c6aac21c
Fausto M. Lagos S <fausto@autonomia.digital>
2023-03-13 12:32:03 -0500

    [FJ] Detached added to docker run

--- docker-run.sh
+++ docker-run.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker run --rm -p 35729:35729 -p 4000:4000 --mount type=bind,src="$(pwd)",target=/home/jekyll/app jekyll-template:latest
+docker run --rm -d -p 35729:35729 -p 4000:4000 --mount type=bind,src="$(pwd)",target=/home/jekyll/app jekyll-template:latest
