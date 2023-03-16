c4f1e3f0139b631c5d3d3463f96b13654984ed54
SofiaSSR <unitargetofsofi@gmail.com>
2023-03-16 11:11:22 -0700

    #3 [N] add docker group to installation script

--- DEV/docker.sh
+++ DEV/docker.sh
@@ -1,4 +1,13 @@
+#in this script we end clean the installation of Docker and create the images for
+
+
+# put docker as service
+systemctl enable docker 
+
+# build docker image for the blog
 docker build -t jekyll-template:latest .
+
+#automate docker
 
 
 
