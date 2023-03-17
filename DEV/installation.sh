33366d486cd4992ce7f553dc6d8ac2cd250cef96
TheComputerCat <jusanchez@unal.edu.co>
2023-03-17 10:41:21 -0500

    #3 [JS] - Tor installation with repos

--- DEV/installation.sh
+++ DEV/installation.sh
@@ -14,7 +14,17 @@ cat <<'END'
 END
 
 # Tor installation
-apt-get install tor -qq > /dev/null
+apt-get install apt-transport-https -qq > /dev/null
+
+echo \
+"deb     [signed-by=/usr/share/keyrings/tor-archive-keyring.gpg] https://deb.torproject.org/torproject.org $(lsb_release -cs) main 
+deb-src [signed-by=/usr/share/keyrings/tor-archive-keyring.gpg] https://deb.torproject.org/torproject.org $(lsb_release -cs) main" | tee /etc/apt/sources.list.d/tor.list > /dev/null
+
+wget -qO- https://deb.torproject.org/torproject.org/A3C4F0F979CAA22CDBA8F512EE8CBC9E886DDD89.asc | gpg --dearmor | tee /usr/share/keyrings/tor-archive-keyring.gpg > /dev/null
+
+
+apt-get update -qq > /dev/null
+apt-get install tor deb.torproject.org-keyring -qq > /dev/null
 cat <<'END'
 +---------------------------------------------+
 + Tor has been successfully installed         +
